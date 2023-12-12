0c7892c0f7a25b18dd67fac4427b08dae1e7c12c
cmorales <caitlyn.morales424@gmail.com>
2023-12-11 19:06:25 -0500

    music

---
+++ oop/Music/music.py
@@ -0,0 +1,7 @@
+class Music:
+    def __init__(self, bpm: int, instruments: str, lyrics: bool):
+        self.bpm = bpm
+        self.instruments = instruments 
+        self.lyrics = lyrics 
+
+    def
