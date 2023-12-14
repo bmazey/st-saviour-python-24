bf295abb035cc5699a572e3b0353c8b5d8e3611c
cmorales <caitlyn.morales424@gmail.com>
2023-12-14 10:14:47 -0500

    updates

--- oop/music/lyrics.py
+++ oop/music/lyrics.py
@@ -2,8 +2,11 @@ from oop.music.music import Music
 
 class Lyrics(Music):
     """a new class that identifies that music has lyrics"""
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, title):
+        super().__init__(title)
 
 # identifies that a song with lyrics is a bool returning True. 
         self.lyrics = True
+
+def play(self) -> str:
+        return 'it is' + self.lyrics + 'that' + self.title + 'has lyrics'
--- oop/music/test_lyrics.py
+++ oop/music/test_lyrics.py
@@ -1,2 +1,8 @@
 from oop.music.music import Music
 from oop.music.music import Lyrics
+
+
+def test_lyrics():
+    listen = Lyrics('rip',True)
+    play = listen.play()
+    assert play == 'it is True that rip has lyrics'
