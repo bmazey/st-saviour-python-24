18b5e12eb01760a8d8b59354724f68bd05732ba0
hflanders <hflanders007@gmail.com>
2023-12-15 09:34:29 -0500

    lyrics

--- oop/music/lyric_songs.py
+++ oop/music/lyric_songs.py
@@ -2,11 +2,12 @@ from oop.music.music import Music
 
 class Lyrics(Music):
     """a new class that identifies music that has lyrics"""
-    def __init__(self, title):
-        super().__init__(title)
-
+    def __init__(self, title: str, bpm: int, instruments: [], lyrics: str, has_lyrics: bool):
+        super().__init__(title, bpm, instruments, True)
+        self.lyrics = lyrics
+        self.has_lyrics = has_lyrics
         # we are teling the code that there should be lyrics in the music in Lyrics
-        self.lyrics = True
+        self.has_lyrics = True
 
-    def play(self) -> str:
-        return 'it is ' + self.lyrics + ' that ' + self.title + ' has lyrics'
+    def sing(self) -> str:
+        return 'the song ' + self.title + ' has the lyrics: ' + self.lyrics
---
+++ oop/music/test_lyric_songs.py
@@ -0,0 +1,10 @@
+from oop.music.lyric_songs import Lyrics
+from oop.music.beats import Lyrics
+
+def test_lyrics():
+    grooves = Lyrics('Here', 120, ['vocals'], 'I ask myself what am I doing here', True)
+    assert grooves.sing() == 'the song Here has the lyrics: I ask myself what am I doing here'
+    assert grooves.play() == 'Here is a song that plays at 120 beats per minute with vocals'
+
+def test_beats():
+    jams = Beats('')
