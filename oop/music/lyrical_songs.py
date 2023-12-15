83b42f66d5a8c89c97a3114fc2f6af0f4ed9fcdd
Eleonor <eleonortiamartin@gmail.com>
2023-12-15 14:28:58 -0500

    spacing

--- oop/music/lyrical_songs.py
+++ oop/music/lyrical_songs.py
@@ -4,6 +4,7 @@ class Lyrical(Music):
 
     def __init__(self, title: str, bpm: int, instruments: [], lyrics: str, has_lyrics: bool):
         super().__init__(title, bpm, instruments, True)
+        
         self.lyrics = lyrics
         self.has_lyrics = has_lyrics
         self.has_lyrics = True 
