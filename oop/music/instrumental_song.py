ad9352435d562e5de08e9580b25ea92c1bb21ea6
Eleonor <eleonortiamartin@gmail.com>
2023-12-15 09:34:10 -0500

    update

--- oop/music/instrumental_song.py
+++ oop/music/instrumental_song.py
@@ -2,8 +2,12 @@ from oop.music.music import Music
 
 class Instrumental(Music):
 
-    def __init__(self, name):
-        super().__init__(name)
-
+    def __init__(self, title: str, bpm: int, instruments: [], leading_instrument: str, has_lyrics: bool):
+        super().__init__(title, bpm, instruments, False)
+        self.leading_instrument = leading_instrument
+        self.has_lyrics = has_lyrics
         self.has_lyrics = False
+
+    def beats(self):
+        return 'the song ' + self.title + ' has a bpm of ' + self.bpm + 
     
--- oop/music/test_lyrics.py
+++ oop/music/test_lyrics.py
@@ -1,7 +1,11 @@
 
 from oop.music.lyrical_songs import Lyrical
+from oop.music.instrumental_song import Instrumental
 
 def test_lyrics():
     last_chrismas = Lyrical('Last Christmas', 60, ['vocals'], 'The very next day you threw it away.', True)
     assert last_chrismas.sing() == 'artist sings Last Christmas with lyrics: The very next day you threw it away.'
     assert last_chrismas.play() == 'This song is called Last Christmas. This song plays at 60 bpm with vocals'
+
+def test_instrumental():
+    cello_song = Instrumental
