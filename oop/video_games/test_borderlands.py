5ae0077b4c9ea04b9b928e2d22437272687d64ba
Jaylynn <jmiri697@gmail.com>
2024-01-09 10:40:36 -0500

    WOOOHOOO

--- oop/video_games/test_borderlands.py
+++ oop/video_games/test_borderlands.py
@@ -1,7 +1,7 @@
 from oop.video_games.borderlands import Borderlands
 
 def test_new_borderlands():
-    skag = Borderlands('Skag')
+    borderlands2 = Borderlands('Borderands 2')
 
     # start by asserting empty arena
     assert enemy.empty_arena()
