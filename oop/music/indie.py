4df17a37a5d57ff2204d3d441abab3bb2a98f273
cmorales <caitlyn.morales424@gmail.com>
2024-01-09 08:39:10 -0500

    yay

--- oop/music/indie.py
+++ oop/music/indie.py
@@ -2,4 +2,11 @@ from oop.music.lyrics import Lyrics
 
 class Indie(Lyrics): 
     """ Is a class that identifies Indie music """
-    def __init__(self, title: str, bpm: int, instruments: [], lyrics: str, has_lyrics)
+    def __init__(self, title: str, bpm: int, instruments: [], lyrics: str, has_lyrics: bool):
+        self.lyrics = lyrics
+        self.has_lyrics = has_lyrics 
+        self.has_lryics = True
+
+def words(self) -> str:
+    return 'it is ' + self.has_lryics + ' that ' + self.title + 'has lyrics'
+        
