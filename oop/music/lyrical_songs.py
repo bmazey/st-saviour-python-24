446510b921df6c751fe33be9184f004860b4904e
Eleonor <eleonortiamartin@gmail.com>
2024-01-11 13:08:47 -0500

    update

--- oop/music/lyrical_songs.py
+++ oop/music/lyrical_songs.py
@@ -3,7 +3,8 @@ from oop.music.music import Music
 class Lyrical(Music):
     """A class that defines music with lyrics"""
 
-    
+    # Assigning previous properties of music to new Lyrical class. 
+    # Adding a string with lyrics
     def __init__(self, title: str, bpm: int, instruments: [], lyrics: str, has_lyrics: bool):
         super().__init__(title, bpm, instruments, True)
         
@@ -11,5 +12,6 @@ class Lyrical(Music):
         self.has_lyrics = has_lyrics
         self.has_lyrics = True 
 
+    # Sing is an instance of Lyrical
     def sing(self):
         return 'artist sings ' + self.title + ' with lyrics: ' + self.lyrics
